0a59d38ddccf6ab75e88c31a0e78b1fd1e72dc61
Jannis Hermanns <jannis@gmail.com>
2011-12-13 12:46:31 +0100

    More docs and changed where loglevel is changed depending on the env

--- lib/init.rb
+++ lib/init.rb
@@ -3,12 +3,6 @@ Bundler.require :default
 
 # Init Goliath env unless it was done already
 Goliath.env rescue Goliath.env = (ENV['RACK_ENV'] || 'development').to_sym
-
-# In dev mode, we do some logging (defaults to Logger::ERROR in other
-# envs)
-if Goliath.env == :development || ENV['BOBS']
-  Tom::Log.logger.level = Logger::INFO
-end
 Tom::Log.logger.info "Started goliath in #{Goliath.env} environment (change with ruby your_app.rb -e development or by setting $RACK_ENV)"
 
 # Fetch the codes
--- lib/tom/log.rb
+++ lib/tom/log.rb
@@ -3,11 +3,20 @@ require 'logger'
 module Tom
   module Log
 
-
+  # Allows you to define your own logger. Tom itself will
+  # log things as .debug, so you can log with .info level
+  # in your app and do stuff. Or you can use your own
+  # logging altogether
+  #
+  # @param [Object] logger Some object that implements the
+  #   .info .debug etc.
+  #  @return [Object] The logger param
   def self.logger=(logger)
     @logger = logger
   end
 
+  # Accessor for the current logger
+  # @return [Object] See {Tom::Log.logger=}
   def self.logger
     unless @logger
       @logger = ::Logger.new(STDOUT)
@@ -16,8 +25,19 @@ module Tom
     @logger
   end
 
+  # Takes the current {Tom::Log.logger} and sets its level
+  # to {Logger::INFO} when you're in the development mode or
+  # when ENV['BOBS'] is set.  Otherwise the log level is set
+  # to {Logger::ERROR}.
+  #
+  # Also, the log format is changed to something short (hh:mm:ss)
+  # @return [void]
   def self.init_logger_defaults
-    @logger.level = ::Logger::ERROR
+    if Goliath.env == :development || ENV['BOBS']
+      Tom::Log.logger.level = Logger::INFO
+    else
+      @logger.level = ::Logger::ERROR
+    end
     @logger.datetime_format = "%H:%M:%S:"
     Logger::Formatter.module_eval(
       %q{ def call(severity, time, progname, msg)} +
