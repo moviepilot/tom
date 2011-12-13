2ae723f811601aebe77dbce47f607627cbdaa3e3
Jannis Hermanns <jannis@gmail.com>
2011-12-13 12:00:30 +0100

    Changed logger

--- lib/tom/log.rb
+++ lib/tom/log.rb
@@ -1,11 +1,56 @@
 require 'logger'
 
 module Tom
-  LOG = ::Logger.new(STDOUT)
-  LOG.level = ::Logger::ERROR
-  LOG.datetime_format = "%H:%M:%S:"
-  Logger::Formatter.module_eval(
-    %q{ def call(severity, time, progname, msg)} +
-    %q{ "#{format_datetime(time)} #{msg2str(msg)}\n" end}
-  )
+  module Log
+
+
+  def self.logger=(logger)
+    @logger = logger
+  end
+
+  def self.logger
+    unless @logger
+      @logger = ::Logger.new(STDOUT)
+      init_logger_defaults
+    end
+    @logger
+  end
+
+  def self.init_logger_defaults
+    @logger.level = ::Logger::ERROR
+    @logger.datetime_format = "%H:%M:%S:"
+    Logger::Formatter.module_eval(
+      %q{ def call(severity, time, progname, msg)} +
+      %q{ "#{format_datetime(time)} #{msg2str(msg)}\n" end}
+    )
+    end
+  end
+end
+require 'logger'
+
+module Tom
+  module Log
+
+
+  def self.logger=(logger)
+    @logger = logger
+  end
+
+  def self.logger
+    unless @logger
+      @logger = ::Logger.new(STDOUT)
+      init_logger_defaults
+    end
+    @logger
+  end
+
+  def self.init_logger_defaults
+    @logger.level = ::Logger::ERROR
+    @logger.datetime_format = "%H:%M:%S:"
+    Logger::Formatter.module_eval(
+      %q{ def call(severity, time, progname, msg)} +
+      %q{ "#{format_datetime(time)} #{msg2str(msg)}\n" end}
+    )
+    end
+  end
 end
